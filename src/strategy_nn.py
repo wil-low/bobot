ce6baa80a430b073ce0a192aeb735bf15a62ffa9
Andrei Ivushkin <aivushkin@gmail.com>
2025-05-08 09:06:17 +0300

    Signal added

--- src/strategy_nn.py
+++ src/strategy_nn.py
@@ -11,11 +11,9 @@ import backtrader as bt
 class AntyCollector(bt.Strategy):
     params = (
         ('trending_bars', 3),
-        ('overbought', 25),
-        ('oversold', 75),
-        ('require_fast_trend', True),
-        ('pivot_lookback', 20),
-        ('min_d_diff', 20),
+        ('overbought', 20),
+        ('oversold', 80),
+        ('require_fast_trend', False),
         ('logger', None),
     )
 
@@ -67,35 +65,72 @@ class AntyCollector(bt.Strategy):
             if perc_d == 0 or perc_d == nan:
                 continue
 
-            # Collect last self.input_length normalized indicator A, B, C values (scale to [0, 1] here if needed)
-            a_values = [self.stoch[d].percK[-3 - i] for i in range(0, self.input_length)]
-            b_values = [self.stoch[d].percD[-3 - i] for i in range(0, self.input_length)]
+            eidx = -3  # entry index
 
-            # Normalize values between 0 and 1 for NN input
-            def normalize(values):
-                min_v, max_v = 0, 100
-                return [(v - min_v) / (max_v - min_v) if max_v > min_v else 0.0 for v in values]
+            fast_up = (self.stoch[d].percK[eidx] - self.stoch[d].percK[eidx - 1] > 0)
+            fast_dir_changed = fast_up != (self.stoch[d].percK[eidx - 1] - self.stoch[d].percK[eidx - 2] > 0)
+            not_over = (
+                self.stoch[d].percD[eidx] < self.params.oversold and
+                self.stoch[d].percD[eidx] > self.params.overbought and
+                self.stoch[d].percK[eidx] < self.params.oversold and
+                self.stoch[d].percK[eidx] > self.params.overbought
+            )
 
-            a_norm = normalize(a_values)
-            b_norm = normalize(b_values)
+            slow_rising = False
+            slow_falling = False
+            fast_was_rising = False
+            fast_was_falling = False
+            signal = 0
+            if fast_dir_changed and not_over:
+                percD = self.stoch[d].percD
+                slow_rising = Anty.is_rising(percD, self.params.trending_bars, -eidx)
+                slow_falling = Anty.is_falling(percD, self.params.trending_bars, -eidx)
+                if self.params.require_fast_trend:
+                    fast_was_rising = Anty.is_rising(self.stoch[d].percK, self.params.trending_bars, 1 - eidx)
+                    fast_was_falling = Anty.is_falling(self.stoch[d].percK, self.params.trending_bars, 1 - eidx)
+                else:
+                    fast_was_rising = True
+                    fast_was_falling = True
+                if slow_rising and fast_up and fast_was_falling:
+                    signal = 1
+                elif slow_falling and not fast_up and fast_was_rising:
+                    signal = -1
 
-            # Check if a_norm or b_norm contain NaN values
-            if any(math.isnan(x) for x in a_norm) or any(math.isnan(x) for x in b_norm):
-                #print("Error: One of the arrays contains NaN values.")
-                continue
+            self.log(d, "%s: o %.5f, h %.5f, l %.5f, c %.5f; %%K %.2f, %%D %.2f; %d/%d/%d, %d/%d, %d/%d; signal %d" %
+                (d.datetime.datetime(0).isoformat(), d.open[0 + eidx], d.high[0 + eidx], d.low[0 + eidx], d.close[0 + eidx], self.stoch[d].percK[0 + eidx], self.stoch[d].percD[0 + eidx],
+                fast_up, fast_dir_changed, not_over, slow_rising, slow_falling, fast_was_rising, fast_was_falling, signal)
+            )
 
-            def assign_outcome(idx):
-                return 1 if d.close[idx] > d.close[-3] else 0
+            if signal != 0:
+                 # Collect last self.input_length normalized indicator A, B, C values (scale to [0, 1] here if needed)
+                a_values = [self.stoch[d].percK[eidx - i] for i in range(0, self.input_length)]
+                b_values = [self.stoch[d].percD[eidx - i] for i in range(0, self.input_length)]
 
-            # Get future price movements
-            up1 = assign_outcome(-2)
-            up2 = assign_outcome(-1)
-            up3 = assign_outcome(-0)
+                # Normalize values between 0 and 1 for NN input
+                def normalize(values):
+                    min_v, max_v = 0, 100
+                    return [(v - min_v) / (max_v - min_v) if max_v > min_v else 0.0 for v in values]
 
-            # Write row to CSV
-            with open(self.output_file, mode='a', newline='') as f:
-                writer = csv.writer(f)
-                writer.writerow([d.ticker] + a_norm + b_norm + [up1, up2, up3])
+                a_norm = normalize(a_values)
+                b_norm = normalize(b_values)
+
+                # Check if a_norm or b_norm contain NaN values
+                if any(math.isnan(x) for x in a_norm) or any(math.isnan(x) for x in b_norm):
+                    #print("Error: One of the arrays contains NaN values.")
+                    continue
+
+                def assign_outcome(idx):
+                    return 1 if d.close[idx] > d.close[eidx] else 0
+
+                # Get future price movements
+                up1 = assign_outcome(-2)
+                up2 = assign_outcome(-1)
+                up3 = assign_outcome(-0)
+
+                # Write row to CSV
+                with open(self.output_file, mode='a', newline='') as f:
+                    writer = csv.writer(f)
+                    writer.writerow([d.ticker] + a_norm + b_norm + [up1, up2, up3])
 
 class Anty(bt.Strategy):
     params = (
@@ -232,12 +267,12 @@ class Anty(bt.Strategy):
                     fast_was_rising = True
                     fast_was_falling = True
                 if slow_rising and fast_up and fast_was_falling:
-                    pivot = self.latest_pivot(percD, True, self.params.pivot_lookback)
+                    pivot = Anty.latest_pivot(percD, True, self.params.pivot_lookback)
                     self.log(d, f"pivot_up {pivot}")
                     if pivot < 10 and (d.high[0] + d.low[0]) / 2 > (d.high[-pivot] + d.low[-pivot]) / 2 and percD[0] - percD[-pivot] > self.params.min_d_diff:
                         signal = 1
                 elif slow_falling and not fast_up and fast_was_rising:
-                    pivot = self.latest_pivot(percD, False, self.params.pivot_lookback)
+                    pivot = Anty.latest_pivot(percD, False, self.params.pivot_lookback)
                     self.log(d, f"pivot_dn {pivot}")
                     if pivot < 10 and (d.high[0] + d.low[0]) / 2 < (d.high[-pivot] + d.low[-pivot]) / 2 and percD[-pivot] - percD[0] > self.params.min_d_diff:
                         signal = -1
