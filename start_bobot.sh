e92871cff8b49c6a2effac11a53704106779d50c
Andrei Ivushkin <aivushkin@gmail.com>
2025-06-05 04:42:01 +0300

    SLEEP_SEC set to 1

--- start_bobot.sh
+++ start_bobot.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+SLEEP_SEC=1
 CMDLINE="python3 src/main.py $1"
 result=1
 while [ $result -ne 0 ]; do
     $CMDLINE
     result=$?
     echo Restarting...
-    sleep 10
+    sleep $SLEEP_SEC
 done
